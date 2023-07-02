39e8da973736fab0d498cab004dc00973b2986de
José Matheus <XxFULLDLCxX@outlook.com>
2023-07-02 17:28:24 -0300

    Implementando post /messages e get /messages

--- src/app.js
+++ src/app.js
@@ -8,10 +8,11 @@ import dayjs from 'dayjs';
 dotenv.config();
 
 class Message {
-  error(res, error) {
-    console.error(error);
-    res.sendStatus(500);
+  error(res, code, err) {
+    if (err) console.error(err);
+    res.sendStatus(code);
   }
+
 };
 const message = new Message();
 
@@ -19,10 +20,8 @@ const mongoClient = new MongoClient(process.env.DATABASE_URL);
 const db = mongoClient.db();
 
 const app = express();
-app.use(cors());
 app.use(express.json());
-
-// {from: 'João', to: 'Todos', text: 'oi galera', type: 'message', time: '20:04:37'}
+app.use(cors());
 
 /* Participants Routes */
 app.post('/participants', async (req, res) => {
@@ -30,15 +29,13 @@ app.post('/participants', async (req, res) => {
   const participants_schema = joi.object({ name: joi.string().required() });
   const validation = participants_schema.validate(req.body, { abortEarly: false });
 
-  if (validation.error) {
-    const errors = validation.error.details.map((detail) => detail.message);
-    return res.status(422).send(errors);
-  }
+  if (validation.error) return message.error(res, 422);
+
   try {
     // Add case-insensitive search for participants later.
     const participant = await db.collection('participants').findOne({ name });
-    console.log(!participant, participant, name);
-    if (participant) return res.sendStatus(409);
+
+    if (participant) return message.error(res, 409);
     else {
       await db.collection('participants').insertOne({ name, lastStatus: Date.now() });
       await db.collection('messages').insertOne({
@@ -46,7 +43,7 @@ app.post('/participants', async (req, res) => {
         to: 'Todos',
         text: 'entra na sala...',
         type: 'status',
-        time: dayjs('HH:mm:ss'),
+        time: dayjs().format('HH:mm:ss'),
       });
       return res.sendStatus(201);
     }
@@ -59,6 +56,51 @@ app.get('/participants', async (req, res) => {
     res.send(participants);
   } catch (error) { message.error(res, error); }
 });
+
+/* Messages Routes */
+app.post('/messages', async (req, res) => {
+  /* {
+    to: "Maria",
+    text: "oi sumida rs",
+    type: "private_message"
+  } */
+  // {from: 'João', to: 'Todos', text: 'oi galera', type: 'message', time: '20:04:37'}
+
+  // const { user: from } = req.headers;
+  const from = Buffer.from(req.headers.user, 'latin1').toString('utf-8');
+
+  const { to, text, type } = req.body;
+  const participants_schema = joi.object({
+    from: joi.string().required(),
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().valid('message', 'private_message')
+  });
+  const validation = participants_schema.validate({ from, to, text, type }, { abortEarly: false });
+
+  if (validation.error || !await db.collection('participants').findOne({ name: from }))
+    return res.sendStatus(422);
+  try {
+    await db.collection('messages').insertOne({ from, to, text, type, time: dayjs().format('HH:mm:ss') });
+    return res.sendStatus(201);
+  } catch (error) { message.error(res, error); }
+});
+
+app.get('/messages', async (req, res) => {
+  const user = Buffer.from(req.headers.user, 'latin1').toString('utf-8');
+  const limit = Number(req.query.limit);
+
+  const messages_schema = joi.object({ user: joi.string().required(), limit: joi.number().integer().min(1).positive() });
+  const validation = messages_schema.validate({ user, limit }, { abortEarly: false });
+
+  if (validation.error) return message.error(res, 422);
+
+  try {
+    const messages = await db.collection('messages').find({ $or: [{ from: user }, { to: user }] }).toArray();
+    res.send(limit ? messages.splice(-limit) : messages);
+  } catch (error) { message.error(res, error); }
+});
+
 
 app.listen(5000, () => {
   console.log('Server is litening on port 5000.');
